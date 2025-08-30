087ea970b10f03a9e131d44210c608d70e9080cf
CemBaskan <cembaskan329@gmail.com>
2025-08-30 23:14:43 +0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Linux! 🚀");
+        System.out.println("Bu proje MIT lisanslıdır ve Linux terminalinde çalıştırılabilir.");
+    }
+}
